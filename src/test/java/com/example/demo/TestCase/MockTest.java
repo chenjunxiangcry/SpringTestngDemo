ef3398cd58b100f342f44a9e3098052b56c5efa9
chenjx <chenjunxiangcry@163.com>
2021-03-24 19:46:02 +0800

    Modify pom file

--- src/test/java/com/example/demo/TestCase/MockTest.java
+++ src/test/java/com/example/demo/TestCase/MockTest.java
@@ -4,6 +4,7 @@ import com.example.demo.HttpUtils.HttpInvoker;
 import com.example.demo.Base.BaseTestCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.IReporter;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -22,5 +23,18 @@ public class MockTest extends BaseTestCase {
        public void testAccessBaidu() throws IOException, URISyntaxException {
             String body = httpInvoker.get("https://www.baidu.com",null,null);
             System.out.println(body);
+
         }
+    @Test
+    public void testAccessSina() throws IOException, URISyntaxException {
+        String body = httpInvoker.get("https://www.sina.com",null,null);
+        System.out.println(body);
+
+    }
+    @Test
+    public void testAccessTencent() throws IOException, URISyntaxException {
+        String body = httpInvoker.get("https://www.qq.com",null,null);
+        System.out.println(body);
+
+    }
 }

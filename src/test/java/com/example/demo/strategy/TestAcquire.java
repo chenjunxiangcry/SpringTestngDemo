496e7f08e37a332b3c387a865a16e94e9ccee510
chenjx <chenjunxiangcry@gmail.com>
2023-04-02 22:21:55 +0800

    test

--- src/test/java/com/example/demo/strategy/TestAcquire.java
+++ src/test/java/com/example/demo/strategy/TestAcquire.java
@@ -7,11 +7,8 @@ import javax.annotation.Resource;
 @Component
 public class TestAcquire {
     @Resource(type=AgreementAquire.class)
-    final PaymentStraegy standardAquire;
+    PaymentStraegy standardAquire;
 
-    public TestAcquire(PaymentStraegy standardAquire) {
-        this.standardAquire = standardAquire;
-    }
 
     public void test(){
         System.out.println("this is testacquire test");
